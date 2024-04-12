55f743c8eb0679b5ea462ad11c84525113fa0799
Sohan R Rane <sohanrane2206@gmail.com>
2024-04-12 20:39:00 +0530

    #pleasecheck

--- Flask.py
+++ Flask.py
@@ -87,6 +87,7 @@ def login():
 @app.route('/forgot_password')
 def forgot_password():
     return render_template('forgot_password.html')
+#please check this once as it is not redirecting to forgot password page
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
